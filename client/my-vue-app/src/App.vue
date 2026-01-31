def8691fb9d20b5dc9248c9a0287b3b71870a908
Michael Yiu <maplepie32@gmail.com>
2026-01-30 16:17:10 -0800

    adding vue files

--- client/my-vue-app/src/App.vue
+++ client/my-vue-app/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
     <h1>Metadata Viewer</h1>
+    <HelloWorld />
     <MetadataList />
   </div>
 </template>
 
 <script>
 import MetadataList from "./components/MetadataList.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  components: { MetadataList },
+  components: { MetadataList, HelloWorld }
 };
 </script>
